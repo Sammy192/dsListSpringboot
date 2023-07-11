35f7f72c17143d1edc771f439ea311db231a8618
Samuel Borges <sammy_rb@hotmail.com>
2023-07-10 21:40:01 -0300

    Project created

---
+++ src/test/java/com/devSuperiorSamuel/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devSuperiorSamuel.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
